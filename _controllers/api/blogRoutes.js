5a799a1b863472011e8cab8ca4b9381760097a35
Damek Hanson <119096607+Damek-H@users.noreply.github.com>
2023-05-17 23:48:15 -0700

    Add files via upload

---
+++ _controllers/api/blogRoutes.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const router = express.Router();
+const models = require('../../_models');
+const withAuth = require('../../utils/auth');
+
+// Create a blog-post
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newBlog = await models.Blogtech.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newBlog);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// Delete a blog-post
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const blogData = await models.Blogtech.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!blogData) {
+      res.status(404).json({ message: 'No project found with this id!' });
+      return;
+    }
+
+    res.status(200).json(blogData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Update a blog-post
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const blogData = await models.Blogtech.update(
+      {
+        title: req.body.title,
+        description: req.body.blogContent,
+      },
+      {
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      }
+    );
+
+    if (!blogData) {
+      res.status(404).json({
+        message: 'No post found with this id',
+      });
+      return;
+    }
+
+    res.json(blogData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
---
+++ _models/Commenttech.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const Blogtech = require('./Blogtech');
+
+class Commenttech extends Model {}
+
+Commenttech.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Usertech',
+        key: 'id',
+      },
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    comment_description: {
+      type: DataTypes.STRING,
+    },
+    blog_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Blogtech',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'commenttech',
+  }
+);
+
+module.exports = Commenttech;
